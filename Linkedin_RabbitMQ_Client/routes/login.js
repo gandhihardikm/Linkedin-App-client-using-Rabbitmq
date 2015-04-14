49dea839cea8ec38c214dc793fc24346898db2b3
Hardik Gandhi <gandhihardikm@gmail.com>
2015-04-13 19:44:47 -0700

    LinkedIn app client side using rabbitmq
    Linkedin application client side using rabbitmq and REST based API

---
+++ Linkedin_RabbitMQ_Client/routes/login.js
@@ -0,0 +1,78 @@
+/**
+ * New node file
+ */
+
+var ejs = require("ejs");
+
+exports.loadloginpage = loadloginpage;
+exports.successLogin = successLogin;
+exports.failLogin = failLogin;
+exports.connections=connections;
+exports.connectionSearchResult = connectionSearchResult;
+
+function connectionSearchResult(req, res) {
+	ejs.renderFile('./views/connectionSearchResult.html', function(err, result) {
+		if (!err) {
+			res.end(result);
+		} else {
+			res.end("An error occured");
+			console.log(err);
+		}
+	}) ;
+}
+
+function connections(req, res) {
+	ejs.renderFile('./views/connections.html', function(err, result) {
+		if (!err) {
+			res.end(result);
+		} else {
+			res.end("An error occured");
+			console.log(err);
+		}
+	}) ;
+}
+
+function loadloginpage(req, res){
+	ejs.renderFile('./views/LoginPage.html', function(err,result){
+		if(!err){
+			res.end(result);
+		}
+		else{
+			res.end("Error Occured!!");
+			console.log(result);
+		}
+	});
+  //res.render('index', { title: 'Express' });
+};	
+
+function successLogin(request,response)
+{
+	
+	ejs.renderFile('./views/homePage.html',function(err, result) {
+        // render on success
+        if (!err) {
+            response.end(result);
+        }
+        // render or error
+        else {
+            response.end('An error occurred');
+            console.log(err);
+        }
+    });
+}
+
+function failLogin(request,response)
+{
+	ejs.renderFile('./views/LoginPage.html',function(err, result) {
+        // render on success
+        if (!err) {
+            response.end(result);
+        }
+        // render or error
+        else {
+            response.end('An error occurred');
+            console.log(err);
+        }
+    });
+}
+
